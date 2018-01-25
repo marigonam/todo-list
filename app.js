a7278fb780886a9daad4c840381992af9869f719
Your Name <you@example.com>
2018-01-25 10:29:09 +0100

    Read, Update and Delete of todos

--- app.js
+++ app.js
@@ -9,10 +9,19 @@ app.set('views', __dirname + '/views');
 //set template engine
 app.set('view engine' , 'ejs');
 
+// body parser middleware
+
+var bodyParser = require('body-parser');
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
 
 //Setup mongodb
 const MongoClient = require ('mongodb').MongoClient;
 const mongoURL = 'mongodb://localhost:27017/todolist';
+const ObjectId = require('mongodb').ObjectId;
 
 //Connecting to MongoDB
 MongoClient.connect(mongoURL, function(err, db) {
@@ -34,24 +43,85 @@ else {
 
 // routes
 app.get('/', function(req, res){
-    res.render("index");
+	
+todos.find().toArray(function(err,docs){
+	
+	if (err) {
+		console.log(err);
+		
+	} else {
+		res.render("Index",{docs: docs});
+	}
+});
+	
 });
 
 app.get('/todos/:id', function(req, res){
-    res.render("show");
-});
+	
+	var id = ObjectId (req.params.id);
+	todos.findOne({_id: id}, function(err,doc)  {  
+	if (err) {
+		console.log(err);
+	}
+	else {
+		
+		 res.render("show", {doc: doc});
+	}
+
+	});
+});	
+	
 
 app.post('/todos/add', function(req, res){
-    res.redirect("/");
+	
+	todos.insert({title: req.body.title, description: req.body.description }, function(err,result){
+		
+	if (err) {
+		console.log(err);
+	} else {
+		
+   res.redirect("/");
+	}
+});
 });
 
 app.get('/todos/edit/:id', function(req, res){
-    res.render("edit");
+	
+	var id = ObjectId(req.params.id);
+	
+todos.findOne({_id: id}, function(err, doc){
+	if(err) {
+		console.log(err);
+	} else {
+		 res.render("edit",{doc:doc} );
+		
+	}
 });
+});
+	
+	
+	
+	
+	
+   
+
 
 app.post('/todos/update/:id', function(req, res){
-    res.redirect("/");
-});
+	var id = ObjectId(req.params.id);
+	
+	todos.updateOne({_id: id}, 
+		{$set: {title: req.body.title , description: req.body.description}}, function(err,result) {
+		if (err) {
+console.log(err);
+		} else {
+			 res.redirect("/");
+		}
+			
+			
+			
+		});
+	
+	});
 
 app.get('/todos/delete/:id', function(req, res){
     res.redirect("/");
